27ce9277f448215babc37c825c26e1b515090b80
[cai.bowen] <admin@caibowen.net>
2018-09-14 17:46:22 +0800

    bugfix

--- routes/common.js
+++ routes/common.js
@@ -6,13 +6,13 @@ import Position from '../controller/common/position';
 
 /**
  * @api {get} /common/getHomeData getHomeData
- * @apiDescription 获取ip位置信息
+ * @apiDescription 获取首页导航栏信息
  * @apiGroup common
  * @apiVersion 1.0.0
  * 
  * @apiSuccess {Number} code 成功状态码
  * @apiSuccess {String} msg 返回信息
- * @apiSuccess {Array} data 返回ip位置信息
+ * @apiSuccess {Array} data 返回首页导航栏信息
  *
  * @apiSuccessExample Success-Response:
  *    HTTP/1.1 200 OK
@@ -26,12 +26,12 @@ import Position from '../controller/common/position';
 router.get('/getHomeData', HomeData.getHomeData);
 /**
  * @api {get} /common/getAddress getAddress
- * @apiDescription 获取首页导航栏信息
+ * @apiDescription 获取ip地理位置信息
  * @apiGroup common
  * @apiVersion 1.0.0
  *
  * @apiSuccess {Number} code 成功状态码
- * @apiSuccess {Object} data 返回首页导航栏信息
+ * @apiSuccess {Object} data 返回ip地理位置信息
  *
  * @apiSuccessExample Success-Response:
  *    HTTP/1.1 200 OK
